d2ecaf31383804e8cd32185b4152415929dca21d
Anmol Jaiswal <68013660+anmolg1997@users.noreply.github.com>
2024-02-04 14:19:33 +0530

    Time: 5 ms (81.23%) | Memory: 44.5 MB (10.21%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,14 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
         
-            String x_str = Integer.toString(x);
-            int len_x = x_str.length();
+            if (x<0) return false;
+            if (x>=0 && x<=9) return true;
 
-            for (int i=0; i<len_x/2; i++){
-                if (x_str.charAt(i)!=x_str.charAt(len_x-i-1)) return false;
+            int rev_x = 0;
+            int sign_x = (x<0) ? -1 : 1;
+            int abs_x = sign_x*x;
+
+            while (abs_x >0) {
+                rev_x = rev_x*10 + abs_x%10;
+                abs_x/=10;
             }
 
-            return true;
-        
+            if (x==rev_x*sign_x) return true;
+            return false;
+
     }
 }
