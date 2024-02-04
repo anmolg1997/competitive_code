856c3a0707e5da688f06311fae79629cc8ce01a2
Anmol Jaiswal <68013660+anmolg1997@users.noreply.github.com>
2024-02-04 14:20:41 +0530

    Time: 4 ms (100.00%) | Memory: 43.5 MB (78.51%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -5,15 +5,14 @@ class Solution {
             if (x>=0 && x<=9) return true;
 
             int rev_x = 0;
-            int sign_x = (x<0) ? -1 : 1;
-            int abs_x = sign_x*x;
+            int abs_x = x;
 
             while (abs_x >0) {
                 rev_x = rev_x*10 + abs_x%10;
                 abs_x/=10;
             }
 
-            if (x==rev_x*sign_x) return true;
+            if (x==rev_x) return true;
             return false;
 
     }
