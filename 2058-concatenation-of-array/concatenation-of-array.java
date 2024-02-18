2b4cdfe2a676f6368620aa9ca07a9ab8b1ce12a4
Anmol Jaiswal <68013660+anmolg1997@users.noreply.github.com>
2024-02-18 09:40:13 +0530

    Update concatenation-of-array.java

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -8,3 +8,15 @@ class Solution {
         return nums2;
     }
 }
+
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] nums2 = new int[2*nums.length];
+        
+        for (int i=0; i<nums2.length; i++){
+            nums2[i] = (i<nums.length) ? nums[i] : nums[i-nums.length];
+        }
+        return nums2;
+    }
+}
