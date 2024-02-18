aed3547bfe5271b94cc2648700b7586108fee186
Anmol Jaiswal <68013660+anmolg1997@users.noreply.github.com>
2024-02-18 10:59:33 +0530

    Update running-sum-of-1d-array.java

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -12,3 +12,28 @@ class Solution {
     }
 
 }
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        
+        int[] runNums = new int[nums.length];
+
+        runningSum(nums, runNums, 0);
+
+        return runNums;
+    }
+
+    public static void runningSum(int[] source, int[] runSource, int ind) {
+
+        if (ind == source.length) return;
+
+        int tmp = 0;
+        for ( int i=0; i<=ind; i++) {
+            tmp+=source[i];
+        }
+
+        runSource[ind] = tmp;
+
+        runningSum(source, runSource, ind+1);
+    }
+}
